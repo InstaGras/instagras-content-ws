b453c7edc6cc73f578e2c4ebd36346fb5bf000f8
Alban GUILLET <albanguillet98@gmail.com>
2019-11-23 16:24:01 +0100

    connection to Scaleway S3 is now Working properly

--- api.js
+++ api.js
@@ -4,30 +4,32 @@ const AWS = require('aws-sdk');
 const fs = require('fs');
 const path = require('path');
 
-//configuring the AWS environment
+//configuring the S3 environment
 AWS.config.update({
     accessKeyId: process.env['s3.access_key_id'],
-    secretAccessKey: process.env['s3.secret_access_key']
-  });
+    secretAccessKey: process.env['s3.secret_access_key'],
+    endpoint: process.env['s3.endpoint'],
+    region: process.env['s3.region']
+});
 
 var s3 = new AWS.S3();
 var filePath = "./test.txt";
 
 //configuring parameters
 var params = {
-  Bucket: process.env['s3.bucket_name'],
-  Body : fs.createReadStream(filePath),
-  Key : "folder/"+Date.now()+"_"+path.basename(filePath)
-};  
+    Bucket: process.env['s3.bucket_name'],
+    Body: fs.createReadStream(filePath),
+    Key: Date.now() + "_" + path.basename(filePath)
+};
 
 s3.upload(params, function (err, data) {
-  //handle error
-  if (err) {
-    console.log("Error", err);
-  }
+    //handle error
+    if (err) {
+        console.log("Error", err);
+    }
 
-  //success
-  if (data) {
-    console.log("Uploaded in:", data.Location);
-  }
+    //success
+    if (data) {
+        console.log("Uploaded in:", data.Location);
+    }
 });
