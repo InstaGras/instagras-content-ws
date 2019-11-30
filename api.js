e4f895a82452b8333b7fc01eedd444ef23847a4f
Alban GUILLET <albanguillet98@gmail.com>
2019-11-30 12:12:06 +0100

    implementing content recuperation

--- api.js
+++ api.js
@@ -16,10 +16,10 @@ var s3 = new AWS.S3();
 var filePath = "./test.txt";
 
 //configuring parameters
-var params = {
+const params = {
     Bucket: process.env['s3.bucket_name'],
     Body: fs.createReadStream(filePath),
-    Key: Date.now() + "_" + path.basename(filePath)
+    Key: path.basename(filePath)
 };
 
 s3.upload(params, function (err, data) {
@@ -33,3 +33,20 @@ s3.upload(params, function (err, data) {
         console.log("Uploaded in:", data.Location);
     }
 });
+
+const paramsGet = {
+    Bucket: process.env['s3.bucket_name'],
+    Key: path.basename(filePath)
+};
+
+s3.getObject(paramsGet, function (err, data) {
+    //handle error
+    if (err) {
+        console.log("Error", err);
+    }
+
+    //success
+    if (data) {
+        console.log("Loaded in:", data.Body);
+    }
+})
